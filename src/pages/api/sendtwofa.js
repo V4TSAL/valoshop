8e43c4a955e24b8ed8d6b0e71485635e809f32b9
vatsa <vatsal2903@gmail.com>
2023-04-22 23:00:07 +0530

    redirect to enter the two factor code if on

--- src/pages/api/sendtwofa.js
+++ src/pages/api/sendtwofa.js
@@ -1,7 +1,7 @@
-export default function handler(req, res) {
-  // Get data submitted in request's body.
+import { Endpoints, Headers, returnCookie, genAsid } from "./utils";
+export default async function handler(req, res) {
   const body = req.body;
-  res.status(200).json({
+  const twofacode = JSON.stringify({
     type: "multifactor",
     code: `${body.twofactor}`,
     rememberDevice: true,
